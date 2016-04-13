ee464232fdaa247883a1404013fb6ad41f62f84c
Anthony O Connor <anthonyoconnor@gmail.com>
2016-04-12 21:17:11 -0700

    Adding @docs with examples

--- lib/issues/table_formatter.ex
+++ lib/issues/table_formatter.ex
@@ -1,6 +1,13 @@
 defmodule Issues.TableFormatter do
   import Enum, only: [ each: 2, map: 2, map_join: 3, max: 1 ]
 
+  @doc """
+  Prints a table view to STDOUT of headers and assocatied data 
+  in columns.
+  
+  The width of the columns are calculated to fit the longest element 
+  in that column
+  """
   def print_table_for_columns(rows, headers) do
     with  data_by_columns  =  split_into_columns(rows, headers),
           column_widths    = widths_of(data_by_columns),
@@ -13,9 +20,29 @@ defmodule Issues.TableFormatter do
     end
   end
 
+  @doc """
+  Returns a binary version (string) of the parameter
+  
+  ## Examples
+    iex> Issues.TableFormatter.printable("a")
+    "a"
+    iex> Issues.TableFormatter.printable(99)
+    "99"
+  """
   def printable(str) when is_binary(str), do: str
   def printable(str), do: to_string(str)
 
+
+  @doc """
+  Give a list of rows and headers returns a list
+  containing lists of the data in each column.
+  
+  ## Example
+    iex> list = [Enum.into([{"a", "1"},{"b", "2"},{"c", "3"}], %{}),
+    ...>  Enum.into([{"a", "4"},{"b", "5"},{"c", "6"}], %{})]
+    iex> Issues.TableFormatter.split_into_columns(list, [ "a", "b", "c" ])
+    [ ["1", "4"], ["2", "5"], ["3", "6"] ]
+  """
   def split_into_columns(rows, headers) do
     for header <- headers do
       for row <- rows, do: printable(row[header])
